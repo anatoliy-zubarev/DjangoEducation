b87835dba2ae8e11bc7d353c0ef8eb485d2ed9e4
anatoliy.zubarev <anatoliy.zubarev@outlook.com>
2020-04-15 20:40:14 +0500

    PizzaShop edu Django

--- DjangoEducation/settings.py
+++ DjangoEducation/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'firtsapp',
     'testurlapp',
     'teststaticapp',
+    'validformapp',
 ]
 
 MIDDLEWARE = [
--- firtsapp/views.py
+++ firtsapp/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from firtsapp.models import Pizza
 from firtsapp.forms import OrderForm
 
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 # Create your views here.
 def home(request):
     pizzas = Pizza.objects.all()
@@ -9,11 +12,18 @@ def home(request):
 
 def pizza_detail(request, pizza_id):
     pizza = get_object_or_404(Pizza, id=pizza_id)
-    form = OrderForm(initial={
+    form = OrderForm(request.POST or None,initial={
         'pizza': pizza
     })
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('{}?sent=True'.format(reverse('pizza-detail', kwargs={'pizza_id': pizza.id})))
+
     return render(request, 'pizza_detail.html', {
         'pizza': pizza,
-        'form': form
+        'form': form,
+        'sent': request.GET.get('sent', False)
     })
 
